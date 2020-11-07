f20efb55288f85ecbd4eaa251cc57b4b00e1fa39
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-07 18:02:42 +0800

    feature
    Dapper

---
+++ src/Dapper.DAL/Dapper.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dapper.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
