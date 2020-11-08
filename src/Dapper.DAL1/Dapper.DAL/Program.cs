89dbf8585f1bebe83b3a22e8cebc6f979ce91648
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-09 01:32:18 +0800

    feature
    .NET Core3.1版本 Dapper

--- src/Dapper.DAL1/Dapper.DAL/Program.cs
+++ src/Dapper.DAL1/Dapper.DAL/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Linq;
 
 namespace Dapper.DAL
 {
@@ -7,13 +8,15 @@ namespace Dapper.DAL
     {
         static void Main(string[] args)
         {
-            var connStr = "";
+            var connStr = "Data Source=.;Initial Catalog=tempdb;Integrated Security=True";
 
-            var provider = System.Data.SqlDbType
-            var provider = System.Data.Common.DbProviderFactories.GetFactory("System")
+            var provider = System.Data.SqlClient.SqlClientFactory.Instance;
+            //var provider = System.Data.Common.DbProviderFactories.GetFactory("System.Data.SqlClient.SqlClientFactory");
 
-            IDbConnection conn;
-            conn.Query
+            IDbConnection conn = provider.CreateConnection();
+            conn.ConnectionString = connStr;
+            var values = conn.Query("SELECT a = 1");
+            Console.WriteLine(values.First().a);
         }
     }
 }
