a4f0e3a114d8ddf70d6bd741f8818a192eedff64
Ganwei_RD-09 <Ganwei_RD-09@LAPTOP-TFRH16I6>
2021-02-23 10:25:12 +0800

    feature: 添加Excel组件

---
+++ src/Dapper.DAL/Dapper.DAL/Excel/Exporter.cs
@@ -0,0 +1,51 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+
+namespace System.Data
+{
+    public static class DataTableExtensions
+    {
+        /// <summary>
+        /// Datable导出成Excel
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="file">导出路径(包括文件名与扩展名)</param>
+        public static void TableToExcel(this DataTable dt, string file)
+        {
+            IWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet("Sheet1");
+
+            //表头  
+            IRow row = sheet.CreateRow(0);
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                ICell cell = row.CreateCell(i);
+                cell.SetCellValue(dt.Columns[i].ColumnName);
+            }
+
+            //数据  
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                IRow row1 = sheet.CreateRow(i + 1);
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    ICell cell = row1.CreateCell(j);
+                    cell.SetCellValue(dt.Rows[i][j].ToString());
+                }
+            }
+
+            //转为字节数组  
+            MemoryStream stream = new MemoryStream();
+            workbook.Write(stream);
+            var buf = stream.ToArray();
+
+            //保存为Excel文件  
+            using (FileStream fs = new FileStream(file, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(buf, 0, buf.Length);
+                fs.Flush();
+            }
+        }
+    }
+}
